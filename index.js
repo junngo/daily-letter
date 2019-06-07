88fc1eab8a8dffb93925832bdca12684b50fa675
junngo <myeongjun.ko@gmail.com>
2019-06-07 22:22:32 +0900

    added middlewares needed this project(helmet, parser, morgan)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -13,6 +17,14 @@ const handleHome = (req, res) =>
 
 const handleProfile = (req, res) =>
     res.send("You are on my profile");
+
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
