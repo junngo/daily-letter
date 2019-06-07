276772e9a8593b4aae8f9bddfda7bb6e8b17e721
junngo <myeongjun.ko@gmail.com>
2019-06-07 22:49:01 +0900

    Routed user(/, /edit, /password)

---
+++ router.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+import express from "express";
+
+export const userRouter = express.Router();
+
+userRouter.get("/", (req, res) => res.send("user index"));
+userRouter.get("/edit", (req, res) => res.send("user edit"));
+userRouter.get("/password", (req, res) => res.send("user profile"));
