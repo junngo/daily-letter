56e5ef6509dc8bce3c538f105e13f98f97b3706d
junngo <myeongjun.ko@gmail.com>
2019-06-13 01:50:09 +0900

    modified more controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,7 @@ export const postJoin = (req, res) => {
 export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
 export const postLogin = (req, res) => res.redirect(routes.home);
 
-export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
+export const logout = (req, res) => res.redirect(routes.home);
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -12,7 +12,15 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const postUpload = (req, res) => {
+    const {
+        body: {file, title, description}
+    } = req;
+
+    res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});
