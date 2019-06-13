afb604e6130495fad2e2ce314388854109d5c315
junngo <myeongjun.ko@gmail.com>
2019-06-13 22:21:52 +0900

    connecting to mongodb and configure dot env

--- db.js
+++ db.js
@@ -1,62 +1,20 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "myeongjun",
-            email: "jun@junlab.com"
-        }
-    },
-    {
-        id: 123393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121,
-            name: "myeongjun",
-            email: "jun@junlab.com"
-        }
-    },
-    {
-        id: 456393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 12,
-          name: "myeongjun",
-          email: "jun@junlab.com"
-        }
-    },
-    {
-        id: 789393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 442,
-          name: "myeongjun",
-          email: "jun@junlab.com"
-        }
-    },
-    {
-        id: 876393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-          id: 112,
-          name: "myeongjun",
-          email: "jun@junlab.com"
-        }
-    },
-  ];
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connect to DB");
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
