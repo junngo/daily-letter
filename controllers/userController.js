cafd9a04e33ad94a3998b09ec3af6c69696fcbfb
junngo <myeongjun.ko@gmail.com>
2019-06-20 23:53:33 +0900

    github log in

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,7 +38,44 @@ export const postLogin = passport.authenticate("local", {
     successRedirect: routes.home
   });
 
-export const logout = (req, res) => res.redirect(routes.home);
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+    // console.log(accessToken, refreshToken, profile, cb);
+    const {
+        _json: { id, avatar_url, name, email }
+    } = profile;
+    
+    try {
+        const user = await User.findOne({ email });
+        
+        if (user) {
+            user.githubId = id;
+            user.save();
+            return cb(null, user);
+        }
+        
+        const newUser = await User.create({
+            email,
+            name,
+            githubId: id,
+            avatarUrl: avatar_url
+        });
+
+        return cb(null, newUser);
+    } catch (error) {
+        return cb(error);
+    }
+};
+
+export const postGithubLogIn = (req, res) => {
+    res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+    req.logout();
+    res.redirect(routes.home);
+}
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
--- passport.js
+++ passport.js
@@ -1,7 +1,22 @@
 import passport from "passport";
 import User from "./models/User";
+import GithubStrategy from "passport-github";
+import { githubLoginCallback } from "./controllers/userController";
+import routes from "./routes";
 
+// local strategy
 passport.use(User.createStrategy());
+
+// github strategy
+passport.use(
+    new GithubStrategy({
+            clientID: process.env.GH_ID,
+            clientSecret: process.env.GH_SECRET,
+            callbackURL: `http://localhost:4000${routes.githubCallback}`
+        },
+        githubLoginCallback
+    )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
