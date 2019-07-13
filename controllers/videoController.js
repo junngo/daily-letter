ddf498b04e35aa2f0e357f6f00eac9c4648434f7
junngo <myeongjun.ko@gmail.com>
2019-07-13 11:24:55 +0900

    api adding a comment

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import routes from "../routes";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
     try {
@@ -59,7 +60,7 @@ export const videoDetail = async (req, res) => {
     // console.log(req.params); 
     
     try {
-        const video = await Video.findById(id).populate("creator");
+        const video = await Video.findById(id).populate("creator").populate("comments");
         // console.log(video);
         res.render("videoDetail", { pageTitle: "Video Detail", video });
     } catch (error) {
@@ -130,6 +131,30 @@ export const postRegisterView = async (req, res) => {
         video.views += 1;
         video.save();
         res.status(200);
+    } catch (error) {
+        res.status(400);
+    } finally {
+        res.end();
+    }
+};
+
+// Add Comment
+export const postAddComment = async (req, res) => {
+    const {
+        params: { id },
+        body: { comment },
+        user
+    } = req;
+    
+    try {
+        const video = await Video.findById(id);
+        const newComment = await Comment.create({
+            text: comment,
+            creator: user.id
+        });
+
+        video.comments.push(newComment.id);
+        video.save();
     } catch (error) {
         res.status(400);
     } finally {
